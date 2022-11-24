af6a0937e048cbec5ca7d77d6bc083de335324ba
Mischa Johal <majohal67@gmail.com>
2022-11-23 16:16:36 -0800

    copied run.py and lane_follow.py, added plate start and stop to run,py for time trials

---
+++ src/controller/node/lane_follow.py
@@ -0,0 +1,200 @@
+import cv2
+import numpy as np
+import logging
+import math
+import datetime
+import sys
+
+_SHOW_IMAGE = False
+
+
+class HandCodedLaneFollower(object):
+
+    def __init__(self, car=None):
+        # logging.info('Creating a HandCodedLaneFollower...')
+        self.car = car
+        self.curr_steering_angle = 90
+
+    def follow_lane(self, frame):
+        # Main entry point of the lane follower
+
+        lane_lines, frame = detect_lane(frame)
+        final_frame = self.steer(frame, lane_lines)
+
+        return final_frame
+
+    def steer(self, frame, lane_lines):
+        logging.debug('steering...')
+        if len(lane_lines) == 0:
+            logging.error('No lane lines detected, nothing to do.')
+            return frame
+
+        new_steering_angle = compute_steering_angle(frame, lane_lines)
+        # self.curr_steering_angle = stabilize_steering_angle(self.curr_steering_angle, new_steering_angle, len(lane_lines))
+
+        if self.car is not None:
+            self.car.front_wheels.turn(self.curr_steering_angle)
+        curr_heading_image = display_heading_line(frame, self.curr_steering_angle)
+
+        return curr_heading_image, new_steering_angle
+
+
+############################
+# Frame processing steps
+############################
+def detect_lane(frame):
+
+    # frame = cv2.GaussianBlur(frame, (11,11), cv2.BORDER_DEFAULT)
+    frame_out = frame
+
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    lower_blue = np.array([0, 0, 100])
+    upper_blue = np.array([255, 0, 255])
+
+    mask = cv2.inRange(hsv, lower_blue, upper_blue)
+
+    # detect edges
+    edges = cv2.Canny(mask, 200, 400)
+
+    # cv2.imshow("Mask", mask)
+    # cv2.imshow("Canny", edges)
+
+    cropped_edges = edges
+
+    # edges = cv2.GaussianBlur(edges, (15,15), cv2.BORDER_DEFAULT)
+    edges = cv2.dilate(edges, np.ones((5,5)), iterations=1)
+
+    # tuning min_threshold, minLineLength, maxLineGap is a trial and error process by hand
+    rho = 1  # precision in pixel, i.e. 1 pixel
+    angle = np.pi / 180  # degree in radian, i.e. 1 degree
+    min_threshold = 40  # minimal of votes
+    line_segments = cv2.HoughLinesP(cropped_edges, rho, angle, min_threshold, np.array([]), minLineLength=100, maxLineGap=20)
+    # lsd = cv2.createLineSegmentDetector(cv2.LSD_REFINE_NONE, 0.2)
+    # line_segments = lsd.detect(mask)[0]
+
+    lines = np.array([[[x1, y1], [x2, y2]] for [[x1,y1,x2,y2]] in line_segments]).astype(int)
+    # lines = lines[np.array([np.linalg.norm(e[1]-e[0]) for e in lines]) > 20]
+
+    frame_out = cv2.addWeighted(frame_out, 0.5, cv2.polylines(np.zeros_like(frame), lines, False, (255,255,255), 2), 0.6, 1)
+    # cv2.imshow("Lines", cv2.polylines(np.zeros(edges.shape), lines, False, (255,100,0), 2))
+    # cv2.imshow("Mask", edges)
+
+    lane_lines = []
+    if line_segments is None:
+        logging.info('No line_segment segments detected')
+        return lane_lines
+
+    height, width, _ = frame.shape
+    left_fit = []
+    left_weights = []
+    right_fit = []
+    right_weights = []
+
+    boundary = 1/3
+    left_region_boundary = width * (1 - boundary)  # left lane line segment should be on left 2/3 of the screen
+    right_region_boundary = width * boundary # right lane line segment should be on left 2/3 of the screen
+
+    for line_segment in line_segments:
+        for x1, y1, x2, y2 in line_segment:
+            if x1 == x2:
+                logging.info('skipping vertical line segment (slope=inf): %s' % line_segment)
+                continue
+            fit = np.polyfit((x1, x2), (y1, y2), 1)
+            slope = fit[0]
+            intercept = fit[1]
+            if np.abs(slope) > 0.15:
+                if slope < 0:
+                    if x1 < left_region_boundary and x2 < left_region_boundary:
+                        left_fit.append((slope, intercept))
+                        left_weights.append(np.sqrt((x2-x1)**2+(y2-y1)**2))
+                else:
+                    if x1 > right_region_boundary and x2 > right_region_boundary:
+                        right_fit.append((slope, intercept))
+                        right_weights.append(np.sqrt((x2-x1)**2+(y2-y1)**2))
+
+    if len(left_fit) > 0:
+        left_fit_average = np.average(left_fit, axis=0, weights=left_weights)
+        lane_lines.append(make_points(frame, left_fit_average))
+
+    if len(right_fit) > 0:
+        right_fit_average = np.average(right_fit, axis=0, weights=right_weights)
+        lane_lines.append(make_points(frame, right_fit_average))
+
+    logging.debug('lane lines: %s' % lane_lines)  # [[[316, 720, 484, 432]], [[1009, 720, 718, 432]]]
+
+    # Display Lines
+    line_width=4
+    line_image = np.zeros_like(frame)
+    if lane_lines is not None:
+        for line in lane_lines:
+            for x1, y1, x2, y2 in line:
+                cv2.line(line_image, (x1, y1), (x2, y2), (255,100,0), line_width)
+    frame_out = cv2.addWeighted(frame_out, 1, line_image, 1, 1)
+
+    heading_image = np.zeros_like(frame)
+    height, width, _ = frame.shape
+
+    if len(lane_lines) == 0:
+        logging.info('No lane lines detected, do nothing')
+        return -90
+
+    height, width, _ = frame.shape
+    if len(lane_lines) == 1:
+        logging.debug('Only detected one lane line, just follow it. %s' % lane_lines[0])
+        x1, _, x2, _ = lane_lines[0][0]
+        # x_offset = x2 - x1
+        x_offset = 0
+    else:
+        _, _, left_x2, _ = lane_lines[0][0]
+        _, _, right_x2, _ = lane_lines[1][0]
+        camera_mid_offset_percent = 0.00 # 0.0 means car pointing to center, -0.03: car is centered to left, +0.03 means car pointing to right
+        mid = int(width / 2 * (1 + camera_mid_offset_percent))
+        x_offset = (left_x2 + right_x2) / 2 - mid
+
+    # find the steering angle, which is angle between navigation direction to end of center line
+    y_offset = int(height / 2)
+
+    steering_angle = math.atan(x_offset / y_offset)  # angle (in radian) to center vertical line
+
+    # figure out the heading line from steering angle
+    # heading line (x1,y1) is always center bottom of the screen
+    # (x2, y2) requires a bit of trigonometry
+
+    # Note: the steering angle of:
+    # 0-89 degree: turn left
+    # 90 degree: going straight
+    # 91-180 degree: turn right 
+    steering_angle_radian = steering_angle
+    x1 = int(width / 2)
+    y1 = height
+    x2 = int(x1 + height / 2 * math.tan(steering_angle_radian ))
+    y2 = int(height / 2)
+
+    cv2.line(heading_image, (x1, y1), (x2, y2), (255,0,100), line_width)
+    frame_out = cv2.addWeighted(frame_out, 1, heading_image, 1, 1)
+
+    return steering_angle, lane_lines, frame_out
+
+############################
+# Utility Functions
+############################
+
+
+
+def length_of_line_segment(line):
+    x1, y1, x2, y2 = line
+    return math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+
+
+
+def make_points(frame, line):
+    height, width, _ = frame.shape
+    slope, intercept = line
+    y1 = height  # bottom of the frame
+    y2 = int(y1 * 1 / 2)  # make points from middle of the frame down
+
+    # bound the coordinates within the frame
+    x1 = max(-width, min(2 * width, int((y1 - intercept) / slope)))
+    x2 = max(-width, min(2 * width, int((y2 - intercept) / slope)))
+    return [[x1, y1, x2, y2]]
--- src/controller/node/run.py
+++ src/controller/node/run.py
@@ -11,6 +11,9 @@ from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 from geometry_msgs.msg import Twist
 import numpy as np
+import time
+
+import lane_follow
 
 GRAYSCALE_THRESHOLD=254
 
@@ -24,6 +27,22 @@ def PID(kp, ki, kd):
     value = kp*error + (I+ki*error) + kd*(error-prev_error)
     prev_error = error
 
+class score_tracker():
+    def __init__(self):
+        self.pub_plate = rospy.Publisher("/license_plate", String, queue_size=4)
+        self.team_name = "test_team"
+        self.team_password = "password"
+        self.register_plate_location = "0"
+        self.stop_plate_location = "-1"
+        self.garbage_plate = "XXXX"
+        # self.pub_clock = rospy.Subscriber("/clock", int)
+
+        # time.sleep(1)
+        # self.pub_plate.publish("test_team,password,0,XXXX")
+
+    def send_plate(self, plate_location, plate_id):
+        self.pub_plate.publish(f"{self.team_name},{self.team_password},{plate_location},{plate_id}")
+
 class line_following:
   def __init__(self):
     # self.image_pub = rospy.Publisher("image_topic_2",Image)
@@ -32,9 +51,11 @@ class line_following:
     self.bridge = CvBridge()
     self.image_sub = rospy.Subscriber("/R1/pi_camera/image_raw",Image,self.callback)
 
-    self.controller = PID(0.7, 0.5, 0)
+    self.lane_follower = lane_follow.HandCodedLaneFollower()
+
+    self.controller = PID(0.7, 0.5, 0.5)
     self.controller.send(None) # Initialize
-    self.error = -0.2
+    self.error = -0
 
   def callback(self,data):
     try:
@@ -45,45 +66,44 @@ class line_following:
 
     (rows,cols,channels) = frame.shape
     # Crop frame to ignore sky
-    frame_crop = frame[-300:-1, 100:1000]
+    # frame_crop = frame[-300:-1, 100:1000]
     # frame_gray = cv2.cvtColor(frame_crop, cv2.COLOR_BGR2GRAY)
-    frame_hsv = cv2.cvtColor(frame_crop, cv2.COLOR_BGR2HSV)
-    _, frame_thresh_sat = cv2.threshold(frame_hsv[:,:,1], 1, 255, cv2.THRESH_BINARY_INV)
-    frame_thresh_val = cv2.inRange(frame_hsv[:,:,2], 80, 90)
-    frame_bin_thresh = np.bitwise_and(frame_thresh_sat, frame_thresh_val)
-    # img_hsv = cv2.cvtColor(frame_crop, cv2.COLOR_BGR2HSV)
-    # saturation = img_hsv[:, :, 1]
-    # _, frame_bin_thresh = cv2.threshold(saturation, 1, 255, cv2.THRESH_BINARY_INV)
-    # _, frame_bin_thresh = cv2.threshold(frame_gray, 80, 255, cv2.THRESH_BINARY)
-    # Compute the average x value of the black pixels (approx position of line)
-    M = cv2.moments(frame_bin_thresh)
-    x = M["m10"] / (M["m00"]+1)
     
+    # lane_lines, frame = self.lane_follower.detectL(frame)
+
+    steer_angle, lane_lines, frame_out = lane_follow.detect_lane(frame)
+
     # Show camera view
-    frame_out = frame
     # frame_out = cv2.circle(frame_out, (int(x), 700), 4, (0, 0, 255), -1)
-    cv2.imshow("Image window", frame)
+    cv2.imshow("Image window", frame_out)
     cv2.waitKey(3)
 
-    # move = Twist()
-    # move.linear.x = 0.1
-    # if not np.isnan(x):
-    #   self.error = (x-400)/800
 
-    # PID_value = self.controller.send(self.error) 
-    # move.angular.z = -PID_value
-    # print(self.error, PID_value)
-    # try:
-    #     self.pub.publish(move) 
-    # except CvBridgeError as e:
-    #     print(e)
+    move = Twist()
+    move.linear.x = 0.1
+
+    PID_value = self.controller.send(steer_angle) 
+    move.angular.z = -PID_value
+    print(steer_angle, PID_value)
+    try:
+        self.pub.publish(move) 
+    except CvBridgeError as e:
+        print(e)
 
 
 def main(args):
-  ic = line_following()
   rospy.init_node('image_converter', anonymous=True)
+  ic = line_following()
+  tracker = score_tracker()
+
+  time.sleep(1) # wait for node init
+  tracker.send_plate(tracker.register_plate_location,tracker.garbage_plate) # register team
+  time.sleep(3) # delay for showcase only - would want to stop once collected all plates in actual comp
+  tracker.send_plate(tracker.stop_plate_location,tracker.garbage_plate) # stop scoring and timer
+  
   try:
     rospy.spin()
+    return
   except KeyboardInterrupt:
     print("Shutting down")
   cv2.destroyAllWindows()
