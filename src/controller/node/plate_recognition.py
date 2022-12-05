ada2eae90fb71310d08b64e208f98de3a8ff7afa
Mischa Johal <majohal67@gmail.com>
2022-12-04 22:30:25 -0800

    increased accepted plate area, TODO: perspective transform on plate

--- src/controller/node/plate_recognition.py
+++ src/controller/node/plate_recognition.py
@@ -19,6 +19,34 @@ import copy
 fourcc = cv.VideoWriter_fourcc(*'MJPG') 
 out = cv.VideoWriter('/home/fizzer/ros_ws/src/controller/node/pictures/output.avi',fourcc,20.0,(1280,720)) # need to be same dimensions as input image
 
+# blue hsv mask values
+uh_plate = 125
+us_plate = 255
+uv_plate = 255
+lh_plate = 118
+ls_plate = 100 # 85 to 100
+lv_plate = 40
+lower_hsv_plate = np.array([lh_plate,ls_plate,lv_plate])
+upper_hsv_plate = np.array([uh_plate,us_plate,uv_plate])
+
+uh = 176 # Upper Hue
+us = 11 # Upper Sat
+uv = 190 # Upper Value
+lh = 105 # Lower Hue
+ls = 5 # Lower Sat
+lv = 90 # Lower Value
+# mb = 1 # Median Blur, 13
+high_cntr_thresh_y = 575
+low_cntr_thresh_y = 450
+high_cntr_thresh_x = 1200
+low_cntr_thresh_x = 5
+plate_cntr_area_low_thresh = 6500 # increasing this helped to only capture plate when close (very close since it's pretty high)
+plate_cntr_area_high_thresh = 100000 # infinite
+lower_hsv = np.array([lh,ls,lv])
+upper_hsv = np.array([uh,us,uv])
+kernel = np.ones((17,17),np.uint8) # dilation kernel
+
+
 class plate_recognizer():
     def __init__(self):
         self.bridge = CvBridge()
@@ -26,25 +54,6 @@ class plate_recognizer():
     
     def callback(self,data):
         
-        uh = 176 # Upper Hue
-        us = 11 # Upper Sat
-        uv = 190 # Upper Value
-        lh = 105 # Lower Hue
-        ls = 5 # Lower Sat
-        lv = 90 # Lower Value
-        # mb = 1 # Median Blur, 13
-        high_cntr_thresh_y = 525
-        low_cntr_thresh_y = 400
-        high_cntr_thresh_x = 1020
-        low_cntr_thresh_x = 200
-        plate_cntr_area_low_thresh = 2750
-        plate_cntr_area_high_thresh = 8000
-
-
-        lower_hsv = np.array([lh,ls,lv])
-        upper_hsv = np.array([uh,us,uv])
-        kernel = np.ones((17,17),np.uint8) # dilation kernel
-
         try:
             img=self.bridge.imgmsg_to_cv2(data, "bgr8")
         except CvBridgeError as e:
@@ -65,6 +74,7 @@ class plate_recognizer():
             for cntr in contours_edge:
                 x_cntr, y_cntr, width_cntr, height_cntr = cv.boundingRect(cntr)
                 # check whether contour in acceptable area of screen
+                #TODO: probably want two areas, one for the plates on the left, one for the plate(s) on the right
                 if y_cntr > low_cntr_thresh_y and y_cntr+height_cntr < high_cntr_thresh_y and x_cntr > low_cntr_thresh_x and x_cntr < high_cntr_thresh_x:
                     largest_contour = cntr
                     plate_cntr_found = True
@@ -82,10 +92,69 @@ class plate_recognizer():
             if largest_contour_area > plate_cntr_area_low_thresh and largest_contour_area < plate_cntr_area_high_thresh:
                 cv.drawContours(img_copy, [largest_contour], -1, (0,255,0),2)
                 plate = img[y:y+height,x:x+width] # crop to isolate plate
-                #TODO: perspective transform would help for plates that are on corners, since we'll catch them right at the edges of our camera           
-                # cv.imshow("plate",plate)
+                cv.imshow("plate",plate)
+                
+                #TODO: perspective transform could help for plates that are on corners, since we'll catch them right at the edges of our camera
+                
+                # mask blue
+                hsv_plate = cv.cvtColor(plate, cv.COLOR_BGR2HSV)
+                mask_plate = cv.inRange(hsv_plate,lower_hsv_plate,upper_hsv_plate)
+                cv.imshow("mask plate",mask_plate)
+
+                # get contours (no erode seems ok)
+                contours_plate, _ = cv.findContours(mask_plate,cv.RETR_TREE,cv.CHAIN_APPROX_SIMPLE)
+                contours_plate = sorted(contours_plate,key=cv.contourArea,reverse=True)
+                plate_copy = copy.deepcopy(plate)
+                cv.drawContours(plate_copy,contours_plate,-1,(0,255,0),1)
+                cv.imshow("contours plate",plate_copy)         
+                
+                # contour's bounding rectangles
+                bounding_rects = []
+                for cntr in contours_plate:
+                    x,y,width,height = cv.boundingRect(cntr)
+                    bounding_rects.append([[x,y,width,height],(x+width * y+height)]) #[[[x,y,w,h],area], [[x,y,w,h],area], [[x,y,w,h],area]]]
+                
+                # remove bounding rectangles if blue edges present
+                plate_width = plate.shape[1]
+                hystersis = 5 # pixels from width of image
+                bounding_rects_cropped = copy.deepcopy(bounding_rects)
+                for i in bounding_rects:
+                    x_i = i[0][0]
+                    y_i = i[0][1]
+                    width_i = i[0][2]
+                    height_i = i[0][3]
+                    if x_i == 0 or x_i + width_i == plate_width or x_i+width_i >= plate_width - hystersis:
+                        bounding_rects_cropped.remove(i)
         
-        out.write(img_copy)
+                # sort bounding rects by area
+                areas = np.asarray([bounding_rects_cropped[i][1] for i in range(len(bounding_rects_cropped))])
+                max_area_indicies = list(reversed(areas.argsort())) # index of max area is 0th element, descending
+                
+                # largest four bounding rectangles
+                plate_rect_display = copy.deepcopy(plate)
+                char_bounding_rects = []
+                for k in range(4):
+                    rect_idx = max_area_indicies[k]
+                    x_i = bounding_rects_cropped[rect_idx][0][0]
+                    y_i = bounding_rects_cropped[rect_idx][0][1]
+                    width_i = bounding_rects_cropped[rect_idx][0][2]
+                    height_i = bounding_rects_cropped[rect_idx][0][3]
+                    char_bounding_rects.append([x_i,y_i,width_i,height_i])
+                    cv.rectangle(plate_rect_display,(x_i,y_i+height_i),(x_i+width_i,y_i),(0,0,255),1) # (top left), (bottom right)
+                
+                char_bounding_rects.sort(key=lambda char_bounding_rects : char_bounding_rects[0]) # sort based on x value, lowest = leftmost = 0th
+                cv.imshow("char bounding rectangles",plate_rect_display)
+
+                # isolate characters
+                for c in range(len(char_bounding_rects)):
+                    x_c = char_bounding_rects[c][0]
+                    y_c = char_bounding_rects[c][1]
+                    width_c = char_bounding_rects[c][2]
+                    height_c = char_bounding_rects[c][3]
+                    padding = 1 # additional pixels to include, for if we crop in too much
+                    char = plate[y_c-padding:y_c+height_c+padding,x_c-padding:x_c+width_c+padding] # cropped char from plate
+
+        # out.write(img_copy)
         cv.imshow("contours with bounds",img_copy)
         cv.waitKey(3)
 
--- src/controller/node/run_timetrials.py
+++ src/controller/node/run_timetrials.py
@@ -11,6 +11,7 @@ from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 from geometry_msgs.msg import Twist
 import numpy as np
+import time
 
 import lane_follow
 
@@ -26,6 +27,22 @@ def PID(kp, ki, kd):
     value = kp*error + (I+ki*error) + kd*(error-prev_error)
     prev_error = error
 
+class score_tracker():
+    def __init__(self):
+        self.pub_plate = rospy.Publisher("/license_plate", String, queue_size=4)
+        self.team_name = "test_team"
+        self.team_password = "password"
+        self.register_plate_location = "0"
+        self.stop_plate_location = "-1"
+        self.garbage_plate = "XXXX"
+        # self.pub_clock = rospy.Subscriber("/clock", int)
+
+        # time.sleep(1)
+        # self.pub_plate.publish("test_team,password,0,XXXX")
+
+    def send_plate(self, plate_location, plate_id):
+        self.pub_plate.publish(f"{self.team_name},{self.team_password},{plate_location},{plate_id}")
+
 class line_following:
   def __init__(self):
     # self.image_pub = rospy.Publisher("image_topic_2",Image)
@@ -36,12 +53,9 @@ class line_following:
 
     self.lane_follower = lane_follow.HandCodedLaneFollower()
 
-    self.angle_control = PID(0.8, 0.5, 0.8)
-    self.angle_control.send(None) # Initialize
-    # self.offset_control = PID(0.7, 0, 0)
-    self.offset_control = PID(0.7, 0.5, 0)
-    self.offset_control.send(None) # Initialize
-    self.PID_value = -0
+    self.controller = PID(0.7, 0.5, 0.5)
+    self.controller.send(None) # Initialize
+    self.error = -0
 
   def callback(self,data):
     try:
@@ -57,7 +71,7 @@ class line_following:
     
     # lane_lines, frame = self.lane_follower.detectL(frame)
 
-    steer_angle, offset, lane_lines, frame_out = lane_follow.detect_lane(frame)
+    steer_angle, lane_lines, frame_out = lane_follow.detect_lane(frame)
 
     # Show camera view
     # frame_out = cv2.circle(frame_out, (int(x), 700), 4, (0, 0, 255), -1)
@@ -68,11 +82,9 @@ class line_following:
     move = Twist()
     move.linear.x = 0.1
 
-    if steer_angle is not None:
-      self.PID_value = self.angle_control.send(steer_angle) + self.offset_control.send(offset)
-      print(f"{steer_angle:.2f}, {offset:.2f}, {self.PID_value:.2f}")
-
-    move.angular.z = -self.PID_value
+    PID_value = self.controller.send(steer_angle) 
+    move.angular.z = -PID_value
+    print(steer_angle, PID_value)
     try:
         self.pub.publish(move) 
     except CvBridgeError as e:
@@ -80,8 +92,15 @@ class line_following:
 
 
 def main(args):
-  ic = line_following()
   rospy.init_node('image_converter', anonymous=True)
+  ic = line_following()
+  tracker = score_tracker()
+
+  time.sleep(1) # wait for node init
+  tracker.send_plate(tracker.register_plate_location,tracker.garbage_plate) # register team
+  time.sleep(3) # delay for showcase only - would want to stop once collected all plates in actual comp
+  tracker.send_plate(tracker.stop_plate_location,tracker.garbage_plate) # stop scoring and timer
+  
   try:
     rospy.spin()
     return
